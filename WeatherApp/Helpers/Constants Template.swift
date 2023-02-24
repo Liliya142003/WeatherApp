b6db4c11602511a740abe07e25ce032f5412860b
Лілія Вельбой <liliya142003@gmail.com>
2023-02-24 21:10:13 +0200

    first commit

---
+++ WeatherApp/Helpers/Constants Template.swift
@@ -0,0 +1,9 @@
+
+//Add your own keys below and unkoment the code
+
+import Foundation
+
+//struct Constants {
+//    static var apiKey =  ""
+//    static let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=\(Constants.apiKey)&units=metric"
+//}
---
+++ WeatherApp/Helpers/Constants.swift
@@ -0,0 +1,6 @@
+
+import Foundation
+struct Constants {
+    static var apiKey =  "8d22ad46c24a56c015f7262ce1e1f6df"
+    static let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=\(Constants.apiKey)&units=metric"
+}
